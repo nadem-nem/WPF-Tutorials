d4a448fc8c75475cdde190c4adf165b364549e65
nadem-nem <38751670+nadem-nem@users.noreply.github.com>
2018-04-27 11:47:29 +0300

    Window Resources Samples

---
+++ WpfTutorialSamples/WpfTutorialSamples/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfTutorialSamples.WPF_Application;
+
+namespace WpfTutorialSamples
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            ResourcesFromCodeBehindSample wnd = new WPF_Application.ResourcesFromCodeBehindSample();
+            if (e.Args.Length == 1)
+                MessageBox.Show("Now opening file: \n\n" + e.Args[0]);
+            wnd.Show();
+        }
+    }
+}
