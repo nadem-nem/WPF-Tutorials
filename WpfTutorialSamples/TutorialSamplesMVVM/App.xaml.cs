9a4a5f3cd1d55914eb18770fd3419b37f9e5e619
nadem-nem <38751670+nadem-nem@users.noreply.github.com>
2018-04-30 17:56:30 +0300

    Data binding. Including exapmple project with MVVM Design Pattern

---
+++ WpfTutorialSamples/TutorialSamplesMVVM/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace TutorialSamplesMVVM
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            TutorialSamplesMVVM.ChangeNotificationSample.ChangeNotificationSample wnd = new ChangeNotificationSample.ChangeNotificationSample();
+            wnd.Show();
+        }
+       
+    }
+}
---
+++ WpfTutorialSamples/WpfTutorialSamples/DataBinding/ChangeNotificationSample.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace WpfTutorialSamples.DataBinding
+{
+    /// <summary>
+    /// Interaction logic for ChangeNotificationSample.xaml
+    /// </summary>
+    public partial class ChangeNotificationSample : Window
+    {
+        private ObservableCollection<User> users = new ObservableCollection<User>();
+
+        public ChangeNotificationSample()
+        {
+            InitializeComponent();
+
+            users.Add(new User() { Name = "John Doe" });
+            users.Add(new User() { Name = "Jane Doe" });
+
+            lbUsers.ItemsSource = users;
+        }
+
+        private void btnAddUser_Click(object sender, RoutedEventArgs e)
+        {
+            users.Add(new User() { Name = "New user" });
+        }
+
+        private void btnChangeUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbUsers.SelectedItem != null)
+                (lbUsers.SelectedItem as User).Name = "Random Name";
+        }
+
+        private void btnDeleteUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbUsers.SelectedItem != null)
+                users.Remove(lbUsers.SelectedItem as User);
+        }
+    }
+
+    public class User : INotifyPropertyChanged
+    {
+        private string name;
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.name != value)
+                {
+                    this.name = value;
+                    this.NotifyPropertyChanged("Name");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
